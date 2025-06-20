aa0d09cab61dbd7d4d9ec41e4e7b607e471d0fb5
ANKIT SINGH <138208455+Ankit101104@users.noreply.github.com>
2025-06-20 17:28:02 +0530

    Update Footer.js

--- frontend/src/components/Layout/Footer.js
+++ frontend/src/components/Layout/Footer.js
@@ -74,7 +74,7 @@ const Footer = () => {
               </Typography>
               <Typography sx={{ color: '#8892b0' }}>
                 Email: contactnexoraz@gmail.com<br />
-                Phone: +91 7357707100<br />
+                Phone: +91 8239336975<br />
                 Address: Parul University, Vadodara
               </Typography>
             </FooterSection>
