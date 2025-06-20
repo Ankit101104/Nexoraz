1b8c4a6b225bbc63159fd26c68c5f0e0347fcfd5
ANKIT SINGH <138208455+Ankit101104@users.noreply.github.com>
2025-06-20 14:09:48 +0530

    Update axios.js

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://nexoraz.onrender.com/api',
+  baseURL: 'https://nexoraz.onrender.com',
   headers: {
     'Content-Type': 'application/json'
   }
